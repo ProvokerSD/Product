783d5bc28bf4df2e8be927029c4cec6de109de14
Provoker <267142410+ProvokerSD@users.noreply.github.com>
2026-03-11 04:05:26 +0400

    Create Example.lua

---
+++ Assets/Main/Example.lua
@@ -0,0 +1,152 @@
+--[[
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⣿⣿⣷⢸⣿⣿⡜⢯⣷⡌⡻⣿⣿⣿⣆⢈⠻⠿⢿⣿⣿⣿⣿⣿⣿⣷⣦⣤⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⡁⢳⣿⣿⣿⣿⣿⣿⡜⣿⣿⣧⢀⢻⣷⠰⠈⢿⣿⣿⣧⢣⠉⠑⠪⢙⠿⠿⠿⠿⠿⠿⠿⠋⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣱⡇⡞⣿⣿⣿⣿⣿⣿⡇⣿⣿⡏⡄⣧⠹⡇⠧⠈⢻⣿⣿⡇⢧⢢⠀⠀⠑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣇⢃⢿⣿⣿⣿⣿⣿⣷⣿⣿⠇⢃⣡⣤⡹⠐⣿⣀⢻⣿⣿⢸⡎⠳⡄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⣾⣿⣿⠘⡸⣿⣿⣿⣿⣿⣿⣿⡿⣰⣿⣿⢟⡷⠈⠋⠃⠎⢿⣿⡏⣿⠀⠘⢆⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡐⢹⣿⣿⡐⢡⢹⣿⣿⣿⣿⡏⣿⢣⣿⣿⡑⠁⠔⠀⠉⠉⠢⡘⣿⡇⣿⡇⠀⡀⠡⡀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⡇⠘⣿⣿⣇⠇⢣⢻⣿⣿⣿⡇⢇⣾⣿⣿⡆⢸⣤⡀⠚⢂⠀⢡⢿⡇⣿⡇⠀⢿⠀⠀⠄⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⠠⠹⣿⣿⡘⣆⢣⠻⣿⣿⢈⣾⣿⣿⣿⣶⣸⣏⢀⣬⣋⡼⣠⢸⢹⣿⡇⢠⣼⠙⡄⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡇⠁⠹⣿⣇⠹⡃⠃⠙⡇⠘⢿⣿⣿⣿⣿⣿⣏⣓⣉⣭⣴⣿⠘⢸⣿⠁⠘⠋⠀⠹⠄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢷⠀⠀⠈⢿⣇⠂⣷⠄⠐⠀⠘⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢠⢸⡏⠀⢀⣠⣴⣾⣿⣶⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⢆⠀⠀⠀⠙⠆⠈⠢⠲⠥⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⡞⣸⠁⠀⢸⣿⣿⣿⣿⣿⣿⡆⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢶⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄⠃⠀⠀⠘⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⢿⣿⣿⣿⣿⡏⠹⣿⣿⡿⠫⠊⠀⠀⠀⣶⠀⢻⣿⣿⣿⣿⡿⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠻⠿⠿⠿⢋⠀⠀⠀⠀⢀⣼⣿⡆⠈⣿⣿⣿⡟⣱⡷⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢁⣁⡀⠨⣛⠿⠶⠄⢀⣠⣾⣿⣿⣷⠀⢹⣿⡟⣴⠈⢃⣶⠔⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⣿⣿⡄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⡄⠈⣿⣿⡿⠀⡀⣿⣷⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢙⠻⣿⣿⢀⠙⠻⠿⣿⣿⣿⣿⣿⣿⡇⠁⣿⠟⡀⠈⣧⢰⣿⠆⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠿⠴⠮⣥⠻⢧⣤⣄⣀⡉⢩⣭⣍⣃⣀⣩⠎⢀⣼⠉⣼⡯⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠑⠁⣛⠓⢒⣒⣢⡭⢁⡈⠿⠿⠟⠹⠛⠁⠀⠀⠀⠰⠃⠂⠀⠀⠀
+ 
+@ ProvokerSD | © COPYRIGHT
+--// Hey, before skidding read — LICENSE ✨.
+]]
+
+getgenv().namehub = "• Name"
+local Library = loadstring(game:HttpGet('https://raw.githubusercontent.com/ProvokerSD/Product/refs/heads/main/Assets/MainUI'))()
+local Main = Library.__init()
+
+--// [MAIN TAB]
+
+local MainTab = Main.create_tab('Tab') --// [TAB CODE]
+
+MainTab.create_title({ --[TITLE CODE]
+    name = '• Title',
+    section = 'left' --[LEFT/RIGHT]
+})
+
+MainTab.create_paragraph({ --[PARAGRAPH]
+    name = 'Paragraph',
+    title = 'Text',
+    section = 'left'
+})
+
+MainTab.create_toggle({ --[1 TAB]
+    name = 'Toggle',
+    flag = 'ToggleEnabled', --[FLAG]
+    section = 'left', 
+    enabled = false, --[TRUE/FALSE] 
+    callback = function(toggled) 
+  
+    end
+})
+
+MainTab.create_description_toggle({ --[2 TAB]
+    name = 'Toggle 2',
+    flag = 'Toggle2Enabled',
+    section = 'left', 
+    description = 'Description', --[DESCRIPTION]
+    enabled = false, 
+    callback = function(toggled) 
+  
+    end
+})
+
+MainTab.create_button({ --[1 BUTTON]
+    name = 'Button',
+    flag = 'ButtonTouched', 
+    section = 'left', 
+    callback = function(touched) 
+        print('Example of How It Works') --[EXAMPLE]
+    end
+})
+
+MainTab.create_description_button({ --[2 BUTTON]
+    name = 'Button 2',
+    flag = 'ButtonTouched', 
+    section = 'left', 
+    description = 'Description',
+    callback = function(touched) 
+
+    end
+})
+
+MainTab.create_slider({ --[SLIDER]
+    name = 'Slider',
+    flag = 'SliderEnabled',
+    section = 'left',
+    value = 1, -- [DEFAULT VALUE]
+    minimum_value = 0, -- [LOWEST VALUE]
+    maximum_value = 5, -- [HIGHEST VALUE]
+    callback = function(SliderEnabled)
+
+    end 
+})
+
+MainTab.create_dropdown({
+    name = 'Dropdown',
+    flag = 'DropdownEnabled',
+    section = 'left',
+    option = 'Dropdown 2',
+    options = {'Dropdown 1', 'Dropdown 2', 'Dropdown 3'},
+    callback = function(selected)
+
+    end
+})
+
+MainTab.create_textbox({ --[TEXT BOX]
+    name = 'Text Box',
+    section = 'left', 
+    value = '', --// SET A DEFAULT VALUE ⚠️:
+                --// USE QUOTES '' FOR PLAYER NAMES PART NAMES, OR ITEM NAMES (E.G. '@USER123', 'SWORDOFPOWER' OR 'PART123' IF YOU ARE MAKING TELEPORT, VIEW PLAYER AND ETC SCRIPT) 
+                --// REMOVE QUOTES FOR NUMBERS (E.G. 600) WHEN SETTING POWER, AMOUNT, OR OTHER NUMERIC VALUES IF YOU ARE MAKING A POWER KICKS OR SOMETHING SIMILAR SCRIPT.
+                --// THIS HELPS THE SCRIPT UNDERSTAND WHAT TYPE OF INPUT YOU ARE ENTERING. (YOU CAN DELETE ME)
+    callback = function(value) 
+        if value ~= '' and value ~= nil then --// [EXAMPLE]
+            print('VALUE:', value)
+        end
+    end
+})
+
+
+MainTab.create_keybind({ --// [KEYBIND]
+    name = 'Keybind',
+    flag = 'KeybindEnabled',
+    section = 'left',
+    keycode = Enum.KeyCode.M, --// [DEFAULT KEY]
+    callback = function(key)
+        
+    end
+})
+
+MainTab.create_title({ 
+    name = '• Title 2',
+    section = 'right' 
+})
+
+MainTab.create_image({ --// [IMAGE]
+    image = 'rbxassetid://ID', -- [PUT HERE IMAGE ID]
+    section = 'right'
+})
+
+MainTab.create_line({ --[LINE]
+    section = 'right'
+})
+
+MainTab.create_verified({ --[VERIFIED]
+    title = 'Text', -- [IF YOU ADD A VERIFICATION SCRIPT, IT WILL LOOK COOL, LIKE 'VERIFIED']
+    section = 'right'
+})
+
+return Main
